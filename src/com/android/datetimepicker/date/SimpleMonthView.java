f845891ad6466869e84eacaecc21952503f1eb10
Rohit Yengisetty <rohit@cyngn.com>
2014-11-26 19:19:24 -0800

    DateTimePicker: Add facilities to customize MonthView's appearance
    Change-Id: I5297e451225d9d8dc2b4dc81fcbda6df1f33b892

--- src/com/android/datetimepicker/date/SimpleMonthView.java
+++ src/com/android/datetimepicker/date/SimpleMonthView.java
@@ -19,7 +19,6 @@ package com.android.datetimepicker.date;
 import android.content.Context;
 import android.graphics.Canvas;
 
-import java.util.Calendar;
 
 class SimpleMonthView extends MonthView {
 
@@ -31,7 +30,7 @@ class SimpleMonthView extends MonthView {
     public void drawMonthDay(Canvas canvas, int year, int month, int day,
             int x, int y, int startX, int stopX, int startY, int stopY) {
         if (mSelectedDay == day) {
-            canvas.drawCircle(x , y - (MINI_DAY_NUMBER_TEXT_SIZE / 3), DAY_SELECTED_CIRCLE_SIZE,
+            canvas.drawCircle(x , y - (mMiniDayNumberTextSize / 3), mDaySelectedCircleSize,
                     mSelectedCirclePaint);
         }
 
